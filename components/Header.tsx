5a61f2ae48ff0ce02bb3987cb254a729b03c91a5
Dinesh Sandaruwan <imdineshsandaru@gmail.com>
2025-12-30 09:04:18 +0530

    updated header

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,7 +14,7 @@ const Header: React.FC<HeaderProps> = ({ onOpenSettings, hasUrl }) => {
           <span className="material-symbols-outlined">celebration</span>
         </div>
         <div>
-          <h2 className="text-lg font-bold leading-tight tracking-tight text-white">Advertising industry get together</h2>
+          <h2 className="text-lg font-bold leading-tight tracking-tight text-white">Advertising Industry Get Together</h2>
           <p className="text-xs text-gray-400">Guest Management</p>
         </div>
       </div>
